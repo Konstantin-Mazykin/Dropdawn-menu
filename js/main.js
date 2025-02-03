b10dcb5eea966c4aaf01137bf1e7e668180dfc50
Konstantin-Mazykin <konstantin.mazykin@gmail.com>
2025-02-03 11:26:10 +0200

    Add animation

--- js/main.js
+++ js/main.js
@@ -1,51 +1,55 @@
-const menuTitle = document.querySelector(".dropdawn-menu__title");
-const menuList = document.querySelector(".dropdawn-menu__list");
-const listItems = document.querySelectorAll(".dropdawn-menu__item");
-const dropdawnInput = document.querySelector(".dropdawn-menu__input");
+const dropdawns = document.querySelectorAll(".dropdawn-menu");
 
-function openMenu() {
-  menuList.classList.toggle("open-menu");
-  menuTitle.classList.toggle("title-pressed");
-}
+dropdawns.forEach((dropdawnWrapper) => {
+  const menuTitle = dropdawnWrapper.querySelector(".dropdawn-menu__title");
+  const menuList = dropdawnWrapper.querySelector(".dropdawn-menu__list");
+  const listItems = menuList.querySelectorAll(".dropdawn-menu__item");
+  const dropdawnInput = dropdawnWrapper.querySelector(".dropdawn-menu__input");
 
-function processingSelectedItem(evant) {
-  evant.stopPropagation();
-  menuTitle.innerText = this.innerText;
-  clearItemSelection();
-  this.classList.add("selected-item");
-  dropdawnInput.value = this.dataset.value;
-  closeMenu();
-}
-
-function closeMenu() {
-  menuList.classList.remove("open-menu");
-  menuTitle.classList.remove("title-pressed");
-}
-
-function clickOutsideDropdawn(evant) {
-  if (evant.target !== menuTitle) {
-    closeMenu();
+  function openMenu() {
+    menuList.classList.toggle("open-menu");
+    menuTitle.classList.toggle("title-pressed");
   }
-}
 
-function keyboardActions(evant) {
-  if (evant.key === "Tab" || evant.key === "Escape") {
-    closeMenu();
+  function processingSelectedItem(evant) {
+    evant.stopPropagation();
+    menuTitle.innerText = this.innerText;
+    clearItemSelection();
+    this.classList.add("selected-item");
+    dropdawnInput.value = this.dataset.value;
+    closeDropdawnMenu();
   }
-}
 
-function clearItemSelection() {
-  listItems.forEach((item) => {
-    item.classList.remove("selected-item");
+  function clearItemSelection() {
+    listItems.forEach((item) => {
+      item.classList.remove("selected-item");
+    });
+  }
+
+  function closeDropdawnMenu() {
+    menuList.classList.remove("open-menu");
+    menuTitle.classList.remove("title-pressed");
+  }
+
+  function clickOutsideDropdawn(evant) {
+    if (evant.target !== menuTitle) {
+      closeDropdawnMenu();
+    }
+  }
+
+  function keyboardActions(evant) {
+    if (evant.key === "Tab" || evant.key === "Escape") {
+      closeDropdawnMenu();
+    }
+  }
+
+  menuTitle.addEventListener("click", openMenu);
+
+  listItems.forEach((listItem) => {
+    listItem.addEventListener("click", processingSelectedItem);
   });
-}
 
-menuTitle.addEventListener("click", openMenu);
+  document.addEventListener("click", clickOutsideDropdawn);
 
-listItems.forEach((listItem) => {
-  listItem.addEventListener("click", processingSelectedItem);
+  document.addEventListener("keydown", keyboardActions);
 });
-
-document.addEventListener("click", clickOutsideDropdawn);
-
-document.addEventListener("keydown", keyboardActions);
